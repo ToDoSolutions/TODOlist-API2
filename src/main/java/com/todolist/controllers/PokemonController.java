0c2e506c06c26a2e1c775ea56bfb4b7aad84d4cd
Alejandro Santiago Félix <alex.0002002@gmail.com>
2022-08-24 21:03:32 +0200

    Aumentado el controller de pokemon.

--- src/main/java/com/todolist/controllers/PokemonController.java
+++ src/main/java/com/todolist/controllers/PokemonController.java
@@ -1,90 +1,48 @@
 package com.todolist.controllers;
 
-import com.google.common.base.Preconditions;
-import com.todolist.dtos.Difficulty;
 import com.todolist.dtos.ShowTask;
-import com.todolist.entity.Task;
-import com.todolist.entity.pokemon.Pokemon;
-import com.todolist.entity.pokemon.Stat;
+import com.todolist.filters.NumberFilter;
+import com.todolist.services.PokemonService;
 import com.todolist.services.TaskService;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Pattern;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/tasks/pokemon")
 public class PokemonController {
 
-    @Value("${pokemon.api.url}")
-    private String startUrl;
+
 
     @Autowired
     private TaskService taskService;
 
-    public Task parsePokemon(String name, String status, String finishedDate, String startDate, Long priority, Integer days) {
-        String url = startUrl + "/pokemon/" + name;
-        RestTemplate restTemplate = new RestTemplate();
-        Pokemon response = restTemplate.getForObject(url, Pokemon.class);
-        StringBuilder types = new StringBuilder();
-        Preconditions.checkArgument(!(finishedDate != null && days != null), "You can't set both finishedDate and days");
-        Preconditions.checkArgument(!(finishedDate == null && days == null), "You must set either finishedDate or days");
-        for (int i = 0; i < response.getTypes().size(); i++) {
-            types.append(response.getTypes().get(i).getType().getName());
-            if (i != response.getTypes().size() - 1)
-                types.append(" - ");
-        }
-        return Task.of("Catch: " + response.getName(), "Type pokemon: " + types, getPokemonAnnotation(response), status,
-                finishedDate == null ?
-                        startDate == null ?
-                                LocalDate.now().plusDays(days).toString() :
-                                LocalDate.parse(startDate, DateTimeFormatter.ISO_LOCAL_DATE).plusDays(days).toString() :
-                        finishedDate,
-                startDate == null ? LocalDate.now().toString() : startDate, priority,
-                getPokemonDifficulty(response).toString());
-    }
+    @Autowired
+    private PokemonService pokemonService;
 
-    private String getPokemonAnnotation(Pokemon pokemon) {
-        if (getAvgStats(pokemon) < 50) {
-            return "easy peasy lemon squeezy, take one pokeball";
-        } else if (getAvgStats(pokemon) < 100) {
-            return "mmmh, you will nead some pokeballs";
-        } else if (getAvgStats(pokemon) < 150) {
-            return "uffff, you must take a great a amount of superballs";
-        } else if (getAvgStats(pokemon) < 200) {
-            return "Yisus, if you do not catch dozens of super balls, you will not be able to catch it.";
-        } else {
-            return "LMFAO, take the entire Pokemon Center in your bag";
-        }
-    }
 
-    private Difficulty getPokemonDifficulty(Pokemon pokemon) {
-        if (getAvgStats(pokemon) < 50)
-            return Difficulty.EASY;
-        else if (getAvgStats(pokemon) < 100)
-            return Difficulty.MEDIUM;
-        else if (getAvgStats(pokemon) < 150)
-            return Difficulty.HARD;
-        else if (getAvgStats(pokemon) < 200)
-            return Difficulty.HARDCORE;
-        else
-            return Difficulty.I_WANT_TO_DIE;
-    }
-
-    private Double getAvgStats(Pokemon pokemon) {
-        return pokemon.getStats().stream().mapToInt(Stat::getBaseStat).average().orElse(0);
-    }
 
     @GetMapping
-    public ShowTask getAllPokemon(@RequestParam Integer generation) {
-        return  null;
+    public List<ShowTask> getAllPokemon(@RequestParam(required = false) NumberFilter hp,
+                                        @RequestParam(required = false) NumberFilter attack,
+                                        @RequestParam(required = false) NumberFilter defense,
+                                        @RequestParam(required = false) NumberFilter specialAttack,
+                                        @RequestParam(required = false) NumberFilter specialDefense,
+                                        @RequestParam(required = false) NumberFilter speed) {
+        return  pokemonService.findAllPokemon().stream()
+                .filter(pokemon -> {
+                    var hp2 = pokemon.getStats().get(0).getBaseStat();
+                    var attack2 = pokemon.getStats().get(1).getBaseStat();
+                    var defense2 = pokemon.getStats().get(2).getBaseStat();
+                    var specialAttack2 = pokemon.getStats().get(3).getBaseStat();
+                    var specialDefense2 = pokemon.getStats().get(4).getBaseStat();
+                    var speed2 = pokemon.getStats().get(5).getBaseStat();
+                    return hp.isValid(Long.valueOf(hp2)) && attack.isValid(Long.valueOf(attack2)) && defense.isValid(Long.valueOf(defense2)) && specialAttack.isValid(Long.valueOf(specialAttack2)) && specialDefense.isValid(Long.valueOf(specialDefense2)) && speed.isValid(Long.valueOf(speed2));
+                }).map(pokemon -> new ShowTask(pokemonService.parsePokemon(null, null, null, null, 0, pokemon))).toList();
     }
 
     @GetMapping("/{name}")
@@ -96,7 +54,7 @@ public class PokemonController {
                                           @RequestParam(defaultValue = "idTask,title,description,status,finishedDate,startDate,annotation,priority,difficulty,duration") String fields,
                                           @RequestParam(required = false) Integer days
     ) {
-        return new ShowTask(parsePokemon(name, status, finishedDate, startDate, priority, days)).getFields(fields);
+        return new ShowTask(pokemonService.findPokemonByName(name, status, finishedDate, startDate, priority, days)).getFields(fields);
     }
 
     @PostMapping("/{name}")
@@ -107,7 +65,7 @@ public class PokemonController {
                                           @RequestParam(required = false) @Max(value = 5, message = "The priority must be between 0 and 5") Long priority,
                                           @RequestParam(defaultValue = "idTask,title,description,status,finishedDate,startDate,annotation,priority,difficulty,duration") String fields,
                                           @RequestParam(required = false) Integer days) {
-        return new ShowTask(taskService.saveTask(parsePokemon(name, status, finishedDate, startDate, priority, days))).getFields(fields);
+        return new ShowTask(taskService.saveTask(pokemonService.findPokemonByName(name, status, finishedDate, startDate, priority, days))).getFields(fields);
     }
 
 
--- src/main/java/com/todolist/services/PokemonService.java
+++ src/main/java/com/todolist/services/PokemonService.java
@@ -1,4 +1,89 @@
 package com.todolist.services;
 
+import com.todolist.dtos.Difficulty;
+import com.todolist.entity.Task;
+import com.todolist.entity.pokemon.AllPokemon;
+import com.todolist.entity.pokemon.Pokemon;
+import com.todolist.entity.pokemon.Stat;
+import com.todolist.exceptions.BadRequestException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Objects;
+
+@Service
 public class PokemonService {
+
+    @Value("${pokemon.api.url}")
+    private String startUrl;
+
+    public Task findPokemonByName(String name, String status, String finishedDate, String startDate, Long priority, Integer days) {
+        String url = startUrl + "/pokemon/" + name;
+        RestTemplate restTemplate = new RestTemplate();
+        Pokemon response = restTemplate.getForObject(url, Pokemon.class);
+        return parsePokemon(status, finishedDate, startDate, priority, days, response);
+    }
+
+    public Task parsePokemon(String status, String finishedDate, String startDate, Long priority, Integer days, Pokemon pokemon) {
+        StringBuilder types = new StringBuilder();
+        if (finishedDate != null && days != null) throw new BadRequestException("You can't use both finishedDate and days");
+        if (finishedDate == null && days == null) throw new BadRequestException("You must set either finishedDate or days");
+        for (int i = 0; i < pokemon.getTypes().size(); i++) {
+            types.append(pokemon.getTypes().get(i).getType().getName());
+            if (i != pokemon.getTypes().size() - 1)
+                types.append(" - ");
+        }
+        return Task.of("Catch: " + pokemon.getName(), "Type pokemon: " + types, getPokemonAnnotation(pokemon), status,
+                finishedDate == null ?
+                        startDate == null ?
+                                LocalDate.now().plusDays(days).toString() :
+                                LocalDate.parse(startDate, DateTimeFormatter.ISO_LOCAL_DATE).plusDays(days).toString() :
+                        finishedDate,
+                startDate == null ? LocalDate.now().toString() : startDate, priority,
+                getPokemonDifficulty(pokemon).toString());
+    }
+
+    private String getPokemonAnnotation(Pokemon pokemon) {
+        if (getAvgStats(pokemon) < 50) {
+            return "easy peasy lemon squeezy, take one pokeball";
+        } else if (getAvgStats(pokemon) < 100) {
+            return "mmmh, you will nead some pokeballs";
+        } else if (getAvgStats(pokemon) < 150) {
+            return "uffff, you must take a great a amount of superballs";
+        } else if (getAvgStats(pokemon) < 200) {
+            return "Yisus, if you do not catch dozens of super balls, you will not be able to catch it.";
+        } else {
+            return "LMFAO, take the entire Pokemon Center in your bag";
+        }
+    }
+
+    private Difficulty getPokemonDifficulty(Pokemon pokemon) {
+        if (getAvgStats(pokemon) < 50)
+            return Difficulty.EASY;
+        else if (getAvgStats(pokemon) < 100)
+            return Difficulty.MEDIUM;
+        else if (getAvgStats(pokemon) < 150)
+            return Difficulty.HARD;
+        else if (getAvgStats(pokemon) < 200)
+            return Difficulty.HARDCORE;
+        else
+            return Difficulty.I_WANT_TO_DIE;
+    }
+
+    private Double getAvgStats(Pokemon pokemon) {
+        return pokemon.getStats().stream().mapToInt(Stat::getBaseStat).average().orElse(0);
+    }
+
+    public List<Pokemon> findAllPokemon() {
+        RestTemplate restTemplate = new RestTemplate();
+        AllPokemon response = restTemplate.getForObject(startUrl + "/pokemon", AllPokemon.class);
+        return response.getResults().stream()
+                .map(url -> restTemplate.getForObject(url.getUrl(), Pokemon.class))
+                .filter(Objects::nonNull)
+                .toList();
+    }
 }
